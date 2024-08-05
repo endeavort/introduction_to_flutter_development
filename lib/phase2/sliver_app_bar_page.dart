48afd1f0d739ef487621b6ba7fd4c33303eb7c99
Takumi OTAKA <takumi-otaka@mygol.jp>
2024-08-06 04:00:58 +0900

    文字サイズをスイッチで切り替えられる機能を追加

--- lib/phase2/sliver_app_bar_page.dart
+++ lib/phase2/sliver_app_bar_page.dart
@@ -10,194 +10,221 @@ class SliverAppBarPage extends StatefulWidget {
 }
 
 class SliverAppBarPageState extends State<SliverAppBarPage> {
+  double _textScale = 1.0;
+  bool _isLarge = false;
+
+  void _toggleLargeFont(bool isLarge) {
+    setState(() {
+      _textScale = isLarge ? 1.2 : 1.0;
+      _isLarge = isLarge;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      // スクロール可能な領域を作成
-      body: CustomScrollView(
-        slivers: [
-          // スクロール度合いに応じて高さが変更できるAppBar
-          SliverAppBar(
-            backgroundColor: Colors.blueAccent.withOpacity(1),
-            floating: true, // スクロール時にAppBarが表示されるようにする
-            pinned: true, // スクロール時にAppBarが固定されるようにする
-            snap: false, // スクロール時にAppBarが一瞬で表示されるようにする
-            expandedHeight: 180, // 展開時の高さ
-            toolbarHeight: 60, // 通常時の高さ
-            flexibleSpace: FlexibleSpaceBar(
-              title: const Text(
-                '記事カテゴリー',
-                style: TextStyle(
-                  color: Colors.white,
+    // デバイス情報にアクセスするために使用
+    return MediaQuery(
+        // テキストスケーリングに関連するプロパティだけが変更
+        data: MediaQuery.of(context)
+            .copyWith(textScaler: TextScaler.linear(_textScale)),
+        child: Scaffold(
+          // スクロール可能な領域を作成
+          body: CustomScrollView(
+            slivers: [
+              // スクロール度合いに応じて高さが変更できるAppBar
+              SliverAppBar(
+                backgroundColor: Colors.blueAccent.withOpacity(1),
+                floating: true, // スクロール時にAppBarが表示されるようにする
+                pinned: true, // スクロール時にAppBarが固定されるようにする
+                snap: false, // スクロール時にAppBarが一瞬で表示されるようにする
+                expandedHeight: 180, // 展開時の高さ
+                toolbarHeight: 60, // 通常時の高さ
+                flexibleSpace: FlexibleSpaceBar(
+                  title: const Text(
+                    '記事カテゴリー',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                  background: Stack(
+                    children: [
+                      SizedBox(
+                        // 親ウィジェットの幅に対して全幅を使用
+                        width: double.infinity,
+                        child: Image.network(
+                          'https://images.pexels.com/photos/267392/pexels-photo-267392.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+                          fit: BoxFit.cover, // 画像を枠に合わせて表示
+                        ),
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          // 線形グラデーション
+                          gradient: LinearGradient(
+                              // グラデーションの開始位置を上部中央に設定
+                              begin: FractionalOffset.topCenter,
+                              // グラデーションの終了位置を下部中央に設定
+                              end: FractionalOffset.bottomCenter,
+                              // グラデーションの各色の位置を設定
+                              stops: const [
+                                0.5,
+                                1
+                              ],
+                              colors: [
+                                // 上部の色
+                                Colors.grey.withOpacity(0),
+                                // 下部の色
+                                Colors.black,
+                              ]),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-              background: Stack(
-                children: [
-                  SizedBox(
-                    // 親ウィジェットの幅に対して全幅を使用
-                    width: double.infinity,
-                    child: Image.network(
-                      'https://images.pexels.com/photos/267392/pexels-photo-267392.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-                      fit: BoxFit.cover, // 画像を枠に合わせて表示
-                    ),
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                      // 線形グラデーション
-                      gradient: LinearGradient(
-                          // グラデーションの開始位置を上部中央に設定
-                          begin: FractionalOffset.topCenter,
-                          // グラデーションの終了位置を下部中央に設定
-                          end: FractionalOffset.bottomCenter,
-                          // グラデーションの各色の位置を設定
-                          stops: const [
-                            0.5,
-                            1
-                          ],
-                          colors: [
-                            // 上部の色
-                            Colors.grey.withOpacity(0),
-                            // 下部の色
-                            Colors.black,
-                          ]),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          // リストをスクロール可能にする
-          SliverList(
-            delegate: SliverChildListDelegate(
-              <Widget>[
-                Container(
-                  color: Colors.grey[200],
-                  child: Padding(
-                    padding: const EdgeInsets.only(
-                        left: 20.0, right: 20.0, bottom: 20.0),
-                    child: Column(
-                      // 左揃え（Rowの場合は上揃え）
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        const SizedBox(
-                          height: 8,
-                        ),
-                        Row(
-                          // 中央揃え
-                          crossAxisAlignment: CrossAxisAlignment.center,
+              // リストをスクロール可能にする
+              SliverList(
+                delegate: SliverChildListDelegate(
+                  <Widget>[
+                    Container(
+                      color: Colors.grey[200],
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                            left: 20.0, right: 20.0, bottom: 20.0),
+                        child: Column(
+                          // 左揃え（Rowの場合は上揃え）
+                          crossAxisAlignment: CrossAxisAlignment.start,
                           children: <Widget>[
-                            // 利用可能な空間を最大限に使用
-                            const Expanded(
-                              child: Text(
-                                'Sept. 29 2020',
-                              ),
+                            const SizedBox(
+                              height: 8,
                             ),
-                            IconButton(
-                              icon: const Icon(
-                                Icons.share,
-                                color: Colors.blueAccent,
+                            Column(children: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  const Text('文字を大きく'),
+                                  Switch(
+                                    value: _isLarge,
+                                    onChanged: _toggleLargeFont, // コールバック関数
+                                  )
+                                ],
                               ),
-                              onPressed: () {},
+                              Row(
+                                // 中央揃え
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: <Widget>[
+                                  // 利用可能な空間を最大限に使用
+                                  const Expanded(
+                                    child: Text(
+                                      'Sept. 29 2020',
+                                    ),
+                                  ),
+                                  IconButton(
+                                    icon: const Icon(
+                                      Icons.share,
+                                      color: Colors.blueAccent,
+                                    ),
+                                    onPressed: () {},
+                                  ),
+                                ],
+                              ),
+                            ]),
+                            const Text(
+                              '記事タイトル記事タイトル記事タイトル記事タイトル記事タイトル記事タイトル',
                             ),
-                          ],
-                        ),
-                        const Text(
-                          '記事タイトル記事タイトル記事タイトル記事タイトル記事タイトル記事タイトル',
-                        ),
-                        // 横線
-                        const Divider(
-                          height: 30,
-                          thickness: 1,
-                        ),
-                        const SizedBox(
-                          height: 8.0,
-                        ),
-                        Row(
-                          children: <Widget>[
-                            CircleAvatar(
-                              backgroundImage: const NetworkImage(
-                                  'https://images.pexels.com/photos/736716/pexels-photo-736716.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'),
-                              radius: 26,
-                              backgroundColor: Colors.grey[200],
+                            // 横線
+                            const Divider(
+                              height: 30,
+                              thickness: 1,
                             ),
                             const SizedBox(
-                              width: 10,
+                              height: 8.0,
                             ),
-                            const Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  '山田 太郎',
+                            Row(
+                              children: <Widget>[
+                                CircleAvatar(
+                                  backgroundImage: const NetworkImage(
+                                      'https://images.pexels.com/photos/736716/pexels-photo-736716.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'),
+                                  radius: 26,
+                                  backgroundColor: Colors.grey[200],
                                 ),
-                                Text(
-                                  'ITジャーナリスト',
+                                const SizedBox(
+                                  width: 10,
+                                ),
+                                const Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      '山田 太郎',
+                                    ),
+                                    Text(
+                                      'ITジャーナリスト',
+                                    ),
+                                  ],
                                 ),
                               ],
                             ),
-                          ],
-                        ),
-                        const SizedBox(
-                          height: 10.0,
-                        ),
-                        const Row(
-                          children: [
-                            Expanded(
-                              child: SizedBox(),
+                            const SizedBox(
+                              height: 10.0,
                             ),
-                            Icon(
-                              Icons.favorite_border,
-                              color: Colors.blueAccent,
+                            const Row(
+                              children: [
+                                Expanded(
+                                  child: SizedBox(),
+                                ),
+                                Icon(
+                                  Icons.favorite_border,
+                                  color: Colors.blueAccent,
+                                ),
+                                SizedBox(
+                                  width: 5.0,
+                                ),
+                                Text(
+                                  '350',
+                                ),
+                                SizedBox(
+                                  width: 16.0,
+                                ),
+                                Icon(
+                                  Icons.comment,
+                                  color: Colors.blueAccent,
+                                ),
+                                SizedBox(
+                                  width: 5.0,
+                                ),
+                                Text(
+                                  '25',
+                                ),
+                              ],
                             ),
-                            SizedBox(
-                              width: 5.0,
+                            const SizedBox(
+                              height: 10.0,
                             ),
-                            Text(
-                              '350',
+                            const Text(
+                              '私は場合おもにその周旋めという事の所を受けですな。もし今日を通用違いはとにかく同じ意見たたほどをさて行くでしょをは懊悩おっしゃれませつで、実際とはできるでななくっです。釣に申し上げたら事はすでに前をちょうどないたた。同時に岡田さんが吟味落語それだけ批評にしでしょ秩序漠然たる一団私か発展にという皆経過たないなたから、このほかは私か人飯にして、大森君の事に主義の私の無論ご汚辱として私手がご誘惑でしようにぼんやりお学問で述べべきでて、毫もさぞ満足としたているたので出ないた。もしくはまたはお主義の向いのはこう馬鹿としたて、その賞には見えですでという辞令にいうておきました。こんな日言葉の日その国家も私ごろをさましかと三宅さんに知れでます、expectsの当時ですというご講演ますうならが、人間の以上が陰に場合までの個人を十月聴こけれどもいるて、それほどの今に描いからこうした中をどうもしましなと行かましのないと、ないたたて全くご個人できるだ訳たなけれた。つまり通りか無事か尊重が流れるなつつ、今ごろ力をしばいるない上を肝不足の場合で見たなけれ。時間でももちろんいうがつけよたますないなけれて、どうも勢い作るば話はまだなかっですものた。しかもご学習が食っからもいならはずんて、下働きでは、さぞこれか愛するからあっせよですた知れせないですと下げて、支はもつからしまいたくっます。いよいよしかるにさえ同時にウォーズウォースというくれますて、そこをも生涯上かも私のお病気はないししまうんまし。私もそのうち発展の事がご記憶はしていでたくますありと、五一の自我にこう重んずるましという諷刺たて、しかしその人の自力をしがらて、だれかを私の名に養成にしていうのたなと反抗云いて批評充たす来ますあっ。教師にそこで大森さんにあるいは一応あるたものなかっでない。岡田さんははっきりろを申し上げて知れでものならずです。（また人で倒さためですなけれたてなかっはいうだなて、）それほど立てるでモーニングに、ジクソンの相場でもするて投げといった、自我の変化も昔の中でもし生きのにありでが説明院しけれどもいれずについてご評語たものなけれ。',
                             ),
-                            SizedBox(
-                              width: 16.0,
-                            ),
-                            Icon(
-                              Icons.comment,
-                              color: Colors.blueAccent,
-                            ),
-                            SizedBox(
-                              width: 5.0,
-                            ),
-                            Text(
-                              '25',
-                            ),
-                          ],
-                        ),
-                        const SizedBox(
-                          height: 10.0,
-                        ),
-                        const Text(
-                          '私は場合おもにその周旋めという事の所を受けですな。もし今日を通用違いはとにかく同じ意見たたほどをさて行くでしょをは懊悩おっしゃれませつで、実際とはできるでななくっです。釣に申し上げたら事はすでに前をちょうどないたた。同時に岡田さんが吟味落語それだけ批評にしでしょ秩序漠然たる一団私か発展にという皆経過たないなたから、このほかは私か人飯にして、大森君の事に主義の私の無論ご汚辱として私手がご誘惑でしようにぼんやりお学問で述べべきでて、毫もさぞ満足としたているたので出ないた。もしくはまたはお主義の向いのはこう馬鹿としたて、その賞には見えですでという辞令にいうておきました。こんな日言葉の日その国家も私ごろをさましかと三宅さんに知れでます、expectsの当時ですというご講演ますうならが、人間の以上が陰に場合までの個人を十月聴こけれどもいるて、それほどの今に描いからこうした中をどうもしましなと行かましのないと、ないたたて全くご個人できるだ訳たなけれた。つまり通りか無事か尊重が流れるなつつ、今ごろ力をしばいるない上を肝不足の場合で見たなけれ。時間でももちろんいうがつけよたますないなけれて、どうも勢い作るば話はまだなかっですものた。しかもご学習が食っからもいならはずんて、下働きでは、さぞこれか愛するからあっせよですた知れせないですと下げて、支はもつからしまいたくっます。いよいよしかるにさえ同時にウォーズウォースというくれますて、そこをも生涯上かも私のお病気はないししまうんまし。私もそのうち発展の事がご記憶はしていでたくますありと、五一の自我にこう重んずるましという諷刺たて、しかしその人の自力をしがらて、だれかを私の名に養成にしていうのたなと反抗云いて批評充たす来ますあっ。教師にそこで大森さんにあるいは一応あるたものなかっでない。岡田さんははっきりろを申し上げて知れでものならずです。（また人で倒さためですなけれたてなかっはいうだなて、）それほど立てるでモーニングに、ジクソンの相場でもするて投げといった、自我の変化も昔の中でもし生きのにありでが説明院しけれどもいれずについてご評語たものなけれ。',
-                        ),
-                        Center(
-                          child: ElevatedButton(
-                            onPressed: () => Navigator.of(context).push(
-                              MaterialPageRoute(
-                                builder: (context) => _ClipRRectArticle(),
+                            Center(
+                              child: ElevatedButton(
+                                onPressed: () => Navigator.of(context).push(
+                                  MaterialPageRoute(
+                                    builder: (context) => _ClipRRectArticle(),
+                                  ),
+                                ),
+                                child: const Text(
+                                  'ぼかしサンプル',
+                                  style: TextStyle(color: Colors.black),
+                                ),
                               ),
                             ),
-                            child: const Text(
-                              'ぼかしサンプル',
-                              style: TextStyle(color: Colors.black),
-                            ),
-                          ),
+                          ],
                         ),
-                      ],
+                      ),
                     ),
-                  ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
-        ],
-      ),
-    );
+        ));
   }
 }
 
